bbce8d0114df53224cf0f2eff22d4cad7081e5ee
Chandan Kushwaha <chandan@enagagepule.com>
2020-06-06 19:07:05 +0530

    API for get file metadata

--- server.js
+++ server.js
@@ -2,6 +2,9 @@
 
 var express = require('express');
 var cors = require('cors');
+// multer imports
+var multer  = require('multer')
+var upload = multer();
 
 // require and use "multer"...
 
@@ -16,6 +19,22 @@ app.get('/', function (req, res) {
 
 app.get('/hello', function(req, res){
   res.json({greetings: "Hello, API"});
+});
+
+// file metadata API endpoint
+// here, upfile should match with form file field
+app.post("/api/fileanalyse", upload.single('upfile'), function(req, res) {
+  const {file} = req;
+  // if file exists return file metadata else return error
+  if(file) {
+    return res.json({
+      name: file.originalname,
+      type: file.mimetype,
+      size: file.size
+    });
+  } else {
+    return res.json({error: "Internal server error"})
+  }
 });
 
 app.listen(process.env.PORT || 3000, function () {
